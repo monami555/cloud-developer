587b1cd36cf3ee5aa0b92e896b4ae5cdfd4cb6b1
Monika Machunik <monika.machunik@gmail.com>
2020-09-25 20:42:20 +0200

    Added Todo CRUD operations

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,34 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const updatedItem = {
+     todoId,
+     ...updatedTodo
+  }
+
+  await docClient.put({
+     TableName: todoTable,
+     Item: updatedItem
+  }).promise()
+
+  return {
+     statusCode: 200,
+     headers: {
+       'Access-Control-Allow-Origin': '*'
+     },
+     body: JSON.stringify({
+       updatedItem
+     })
+   }
 }
