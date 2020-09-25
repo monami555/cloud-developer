4653bd7a653b8a27d40886897980d6b016df0b44
Monika Machunik <monika.machunik@gmail.com>
2020-09-25 21:26:32 +0200

    Added attachment upload backend

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -2,9 +2,29 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import * as AWS  from 'aws-sdk'
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const s3 = new AWS.S3({
+     signatureVersion: 'v4' // Use Sigv4 algorithm
+  })
+
+  const presignedUrl = s3.getSignedUrl('putObject', { // The URL will allow to perform the PUT operation
+    Bucket: process.env.TODO_ATTACHMENTS_S3, // Name of an S3 bucket
+    Key: todoId, // id of an object this URL allows access to
+    Expires: process.env.SIGNED_URL_EXPIRATION  // A URL is only valid for this time
+  })
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      todoId,
+      presignedUrl: presignedUrl
+    })
+  }
 }
